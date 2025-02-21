ae37202eb0ea50ff590839d63323e28980952b3d
ebrahim.azizi@axelsw.it <ebrahim.azizi@axelsw.it>
2025-02-21 16:54:06 +0100

    Change with some modification in sampling rate and windowing process

--- FieldEffect/main.py
+++ FieldEffect/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from init import *
-from utlis import *
+from utils import *
 
 if __name__ == '__main__':
 
@@ -8,59 +8,73 @@ if __name__ == '__main__':
     f = data.fieldFreq
     Ms = data.Ms
     cycs = data.nPeriod
-    data.rsol = 5000
-    rsol = data.rsol
     num = data.nParticle
-    fs = rsol*2*f
-    dt = 1/fs
-    tf = cycs*(1/f)
-    lent = int(np.ceil(tf/dt))
-    t = np.array([i*dt for i in range(lent)])
+    fieldAml_list = 1e-3*np.array([20, 15, 10, 5]) # field ampl does not affect fs so lent is constant
 
     # data set: magnetic particle core size = 25 nm
-    data.dCore = 25e-9
-    data.dHyd = data.dCore + 10e-9
-    fieldAml_list = 1e-3*np.array([20, 15, 10, 5])
+    dco = 25e-9
+    dhyd = dco+10e-9
+    data.dCore = dco
+    data.dHyd = dhyd
+    params = MPI_Langevin_std_init(data)
+    dt = 1/params.fs
+    tf = params.tf
+    lent = params.lent
+    t = np.array([i*dt for i in range(lent)])
     M = np.zeros( (len(fieldAml_list), lent) )
     N = np.zeros( (len(fieldAml_list), lent) )
     sigH = np.zeros( (len(fieldAml_list), lent) )
+    print(f"simulation for dco = {dco} is started", flush=True)
     for i, a in enumerate(fieldAml_list):
         data.fieldAmpl = a
-        M[i, :], N[i, :], sigH[i,:] = CombinedNeelBrown(data)
+        M[i, :], N[i, :], sigH[i,:] = MPI_Langevin_std_core(data)
         print('\r', 'fieldAmplitued_list: ' + "." * 10 + " ", end=str(i)+'/'+str(len(fieldAml_list)-1))
-    print()
+    print(f"\nsimulation for dco = {dco} is done", flush=True)
     np.savetxt('FieldEffect/data/Core25_M.csv', M, delimiter=',', comments='')
     np.savetxt('FieldEffect/data/Core25_N.csv', N, delimiter=',', comments='')
     np.savetxt('FieldEffect/data/Core25_sigH.csv', sigH, delimiter=',', comments='')
 
     # data set: magnetic particle core size = 30 nm
-    data.dCore = 30e-9
-    data.dHyd = data.dCore + 10e-9
-    fieldAml_list = 1e-3*np.array([20, 15, 10, 5])
+    dco = 30e-9
+    dhyd = dco+10e-9
+    data.dCore = dco
+    data.dHyd = dhyd
+    params = MPI_Langevin_std_init(data)
+    dt = 1/params.fs
+    tf = params.tf
+    lent = params.lent
+    t = np.array([i*dt for i in range(lent)])
     M = np.zeros( (len(fieldAml_list), lent) )
     N = np.zeros( (len(fieldAml_list), lent) )
     sigH = np.zeros( (len(fieldAml_list), lent) )
+    print(f"simulation for dco = {dco} is started", flush=True)
     for i, a in enumerate(fieldAml_list):
         data.fieldAmpl = a
-        M[i, :], N[i, :], sigH[i,:] = CombinedNeelBrown(data)
+        M[i, :], N[i, :], sigH[i,:] = MPI_Langevin_std_core(data)
         print('\r', 'fieldAmplitued_list: ' + "." * 10 + " ", end=str(i)+'/'+str(len(fieldAml_list)-1))
-    print()
+    print(f"\nsimulation for dco = {dco} is done", flush=True)
     np.savetxt('FieldEffect/data/Core30_M.csv', M, delimiter=',', comments='')
     np.savetxt('FieldEffect/data/Core30_N.csv', N, delimiter=',', comments='')
     np.savetxt('FieldEffect/data/Core30_sigH.csv', sigH, delimiter=',', comments='')
 
     # data set: magnetic particle core size = 35 nm
-    data.dCore = 35e-9
-    data.dHyd = data.dCore + 10e-9
-    fieldAml_list = 1e-3*np.array([20, 15, 10, 5])
+    dco = 35e-9
+    dhyd = dco+10e-9
+    data.dCore = dco
+    data.dHyd = dhyd
+    params = MPI_Langevin_std_init(data)
+    dt = 1/params.fs
+    tf = params.tf
+    lent = params.lent
     M = np.zeros( (len(fieldAml_list), lent) )
     N = np.zeros( (len(fieldAml_list), lent) )
     sigH = np.zeros( (len(fieldAml_list), lent) )
+    print(f"simulation for dco = {dco} is started", flush=True)
     for i, a in enumerate(fieldAml_list):
         data.fieldAmpl = a
-        M[i,:], N[i, :], sigH[i,:] = CombinedNeelBrown(data)
+        M[i,:], N[i, :], sigH[i,:] = MPI_Langevin_std_core(data)
         print('\r', 'fieldAmplitued_list: ' + "." * 10 + " ", end=str(i)+'/'+str(len(fieldAml_list)-1))
-    print()
+    print(f"\nsimulation for dco = {dco} is done", flush=True)
     np.savetxt('FieldEffect/data/Core35_M.csv', M, delimiter=',', comments='')
     np.savetxt('FieldEffect/data/Core35_N.csv', N, delimiter=',', comments='')
     np.savetxt('FieldEffect/data/Core35_sigH.csv', sigH, delimiter=',', comments='')
